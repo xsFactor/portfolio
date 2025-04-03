20da35970cbab1bef89ba5f51ff634669a256301
xsFactor <115862331+xsFactor@users.noreply.github.com>
2025-04-04 00:53:54 +0100

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,8 +1,8 @@
 import resume from '../assets/pdf/resume.pdf'
 
 export const headerData = {
-    name: 'Habeeb Ahmed',
-    title: "Resilient Intelligence Architect (Cyber, OSINT, Threats) | OSINT Investigator | ",
+    name: 'Habeeb A',
+    title: "Resilient Intelligence Architect (Cyber, OSINT, Threats) | Vulnerability Management Enginner | Instructor | AI Masterpiece",
     desciption:"The adversary evolves, the landscape shifts, but I remain relentless. Every challenge is a puzzle, every setback a lesson. In cybersecurity and intelligence, resilience isn't an option—it's the foundation. I don't just adapt; I anticipate, learn, and fortify. Preparedness is my edge, and problem-solving is my craft. ",
     image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
     resumePdf: resume
