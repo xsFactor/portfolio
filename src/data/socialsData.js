00543a3c405a7cb364749dd4b6843bc6fbb9f33e
xsFactor <115862331+xsFactor@users.noreply.github.com>
2025-06-09 21:28:07 +0100

    Update socialsData.js

--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -2,13 +2,13 @@ export const socialsData = {
     github: 'https://github.com/BrainARD-hub/',
     facebook: 'https://www.facebook.com/',
     linkedIn: 'https://www.linkedin.com/in/ahmedfactor/',
-    instagram: 'https://www.instagram.com/',
+    instagram: 'https://www.instagram.com/civicmilcyber/',
     codepen: 'https://codepen.io/',
     twitter: 'https://x.com/_ahmedhabeeb/',
-    reddit: 'https://www.reddit.com/user/',
+    reddit: 'https://www.reddit.com/user/civicmilcyber/',
     blogger: 'https://www.blogger.com/',
-    medium: 'https://medium.com/@',
+    medium: 'https://medium.com/@CivicMilCyber/',
     stackOverflow: 'https://stackoverflow.com/users/',
     gitlab: 'https://gitlab.com/',
-    youtube: 'https://youtube.com/'
+    youtube: 'https://www.youtube.com/@CivicMilCyber/'
 }
