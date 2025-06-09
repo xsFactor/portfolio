d90183edafdc183f1bc5e2a8f57b94e9b92aff74
xsFactor <115862331+xsFactor@users.noreply.github.com>
2025-06-09 18:48:31 +0100

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Habeeb A',
     title: "Resilient Intelligence Architect (Cyber, OSINT, Threats) | Vulnerability Management Enginner | Instructor | AI Masterpiece",
     desciption:"The adversary evolves, the landscape shifts, but I remain relentless. Every challenge is a puzzle, every setback a lesson. In cybersecurity and intelligence, resilience isn't an option—it's the foundation. I don't just adapt; I anticipate, learn, and fortify. Preparedness is my edge, and problem-solving is my craft. ",
-    image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
+    image: 'https://raw.githubusercontent.com/xsFactor/asset-container/blob/main/developer-portfolio/IMG_4798.JPG',
     resumePdf: resume
 }
