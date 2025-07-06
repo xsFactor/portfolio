85a85005348e755fa8ecfe8fbffcc8c973397aab
xsFactor <115862331+xsFactor@users.noreply.github.com>
2025-07-06 19:34:12 +0100

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,5 +1,5 @@
 export const aboutData = {
     title: "Who I am",
     description1: "My name's Habeeb. I'm a Cybersecurity Instructor, Offensive Security Analyst, and Consultant based in Nigeria. I specialize in penetration testing, phishing simulation, and red teaming, delivering contract-based offensive security services to SMEs. I also design and lead Security Awareness Training (SAT) programs tailored to different organizational needs. As an instructor, I train aspiring professionals through hands-on labs and real-world projects in VAPT, OSINT, and digital forensics. I regularly consult for NGOs and private firms, using OSINT to uncover critical insights and support investigations. I'm passionate about offensive security, cyber education, and building scalable security solutions for businesses and communities alike.",
-    image: 2
+    image: 1
 }
