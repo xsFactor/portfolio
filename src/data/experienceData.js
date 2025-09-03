28734ade30b1fefc3a341940f2e8f6a7c4911e32
xsFactor <115862331+xsFactor@users.noreply.github.com>
2025-09-03 23:37:11 +0100

    Update experienceData.js

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,23 +1,30 @@
 export const experienceData = [
     {
         id: 1,
-        company: 'Globex Corporation',
-        jobtitle: 'Frontend Developer',
-        startYear: '2018',
-        endYear: '2019'
+        company: 'Ministry of Justice',
+        jobtitle: 'Confidential',
+        startYear: '2025',
+        endYear: 'Present'
     },
     {
         id: 2,
-        company: 'Vehement Capital Partners',
-        jobtitle: 'Backend Developer',
-        startYear: '2019',
-        endYear: '2020'
+        company: 'PQP Marketing Consult',
+        jobtitle: 'Lead Cybersecurity Facilitator',
+        startYear: '2024',
+        endYear: '2025'
     },
     {
         id: 3,
-        company: 'Wonka Industries',
-        jobtitle: 'Marketing',
-        startYear: '2020',
-        endYear: 'Present'
+        company: 'Nigerian Communication Satellite Limited',
+        jobtitle: 'CTI (Intern)',
+        startYear: '2022',
+        endYear: '2023'
+    },
+    {
+        id: 3,
+        company: 'Nigerian Communication Satellite Limited',
+        jobtitle: 'CTI (Intern)',
+        startYear: '2022',
+        endYear: '2023'
     },
 ]
